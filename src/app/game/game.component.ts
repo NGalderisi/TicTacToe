7d95a075ed2676b0197076594f2c36e91375d152
NGalderisi <nathan@galderisi.com>
2022-06-09 12:15:38 -0400

    Last of the instructions on the tutorial video followed. There are some issues from here that I will work to resolve

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {Gamelogic} from "../gamelogic";
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
+  providers: [Gamelogic]
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(public game: Gamelogic) { }
 
   ngOnInit(): void {
   }
 
+  startGame(): void{
+    this.game.gameStart();
+    const currentPlayer = 'Current turn: Player: ' + this.game.currentTurn;
+    const information = document.querySelector('.current-status');
+    information!.innerHTML = currentPlayer;
+  }
+
+  async clickSubField( subfield: any ): Promise<void> {
+    if (this.game.gameStatus === 1){
+      const position = subfield.currentTarget.getAttribute('position')
+      const information = document.querySelector('.current-status');
+
+      this.game.setField(position, this.game.currentTurn);
+      const color = this.game.getPlayerColorClass();
+      subfield.currentTarget.classList.add(color);
+
+      await this.game.checkForWinner().then( (end: boolean) =>{
+        if ( this.game.gameStatus === 0 && end ){
+          information!.innerHTML = 'The Winner is Player ' + this.game.currentTurn;
+        }
+      });
+
+      await this.game.checkForFull().then( (end: boolean) =>{
+        if ( this.game.gameStatus === 0 && end ){
+          information!.innerHTML = 'Draw'
+        }
+      });
+
+      this.game.changePlayer();
+
+      if(this.game.gameStatus === 1) {
+        const currentPlayer = 'Current turn: Player: ' + this.game.currentTurn;
+        information!.innerHTML = currentPlayer;
+      }
+    }
+  }
+
 }
+
---
+++ src/app/gamelogic.ts
@@ -0,0 +1,107 @@
+import {Status} from './gamestatus';
+
+export class Gamelogic {
+
+    gameField: Array<number> = [];
+
+    currentTurn!: number;
+
+    gameStatus: Status;
+
+    winSituationsOne: Array<Array<number>> = [
+        [1,1,1,0,0,0,0,0,0],
+        [0,0,0,1,1,1,0,0,0],
+        [0,0,0,0,0,0,1,1,1],
+        [1,0,0,1,0,0,1,0,0],
+        [0,1,0,0,1,0,0,1,0],
+        [0,0,1,0,0,1,0,0,1],
+        [1,0,0,0,1,0,0,0,1],
+        [0,0,1,0,1,0,1,0,0]
+    ]
+
+    winSituationsTwo: Array<Array<number>> = [
+        [2,2,2,0,0,0,0,0,0],
+        [0,0,0,2,2,2,0,0,0],
+        [0,0,0,0,0,0,2,2,2],
+        [2,0,0,2,0,0,2,0,0],
+        [0,2,0,0,2,0,0,2,0],
+        [0,0,2,0,0,2,0,0,2],
+        [2,0,0,0,2,0,0,0,2],
+        [0,0,2,0,2,0,2,0,0]
+    ]
+
+    public constructor() {
+        this.gameStatus = Status.STOP
+        this.gameField= [0,0,0,0,0,0,0,0,0]
+    }
+
+    gameStart(): void {
+        this.gameField= [0,0,0,0,0,0,0,0,0]
+        this.currentTurn = this.randomPlayerStart();
+        this.gameStatus= Status.START;
+    }
+
+    randomPlayerStart(): number {
+        const startPlayer = Math.floor(Math.random()*2) + 1;
+        return startPlayer;
+    }
+
+    setField(position: number, value: number): void {
+        this.gameField[position] = value;
+    }
+
+    getPlayerColorClass(): string {
+        const colorClass = (this.currentTurn === 2)? 'player2' : "player1";
+        return colorClass;
+    }
+
+    changePlayer(): void {
+        this.currentTurn = (this.currentTurn === 2)? 1 : 2;
+    }
+
+    async checkForFull(): Promise<boolean> {
+        let isFull = true;
+
+        if (this.gameField.includes(0) ) {
+            isFull = false;
+        }
+        else{
+            this.gameEnd();
+        }
+
+        return isFull;
+    }
+
+    arrayEquals(a: Array<any>, b: Array<any>): boolean{
+        return Array.isArray(a) && Array.isArray(b) && a.length === b.length && a.every ( (value, index) => value === b[index]);
+
+    }
+
+    async checkForWinner(): Promise<boolean> {
+        let isWinner = false;
+
+        const checkArray = (this.currentTurn === 1)? this.winSituationsOne :this.winSituationsTwo;
+
+        const currentarray: any[] = [];
+
+        this.gameField.forEach( (subField,index) =>{
+            if ( subField !== this.currentTurn) {
+                currentarray[index] = 0;
+            } else {
+                currentarray[index] = subField;
+            }
+        });
+
+        checkArray.forEach((checkField, checkIndex) =>{
+            if(this.arrayEquals(checkField,currentarray)){
+                isWinner = true;
+                this.gameEnd();
+            }
+        })
+        return isWinner;
+    }
+
+    gameEnd(): void {
+        this.gameStatus=Status.STOP;
+    }
+}
