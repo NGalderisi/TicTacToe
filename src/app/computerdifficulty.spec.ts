5d5cd2d2dbb059b15e5c3ecb42c035e03e343330
NGalderisi <nathan@galderisi.com>
2022-08-05 11:16:41 -0400

    Removing Unit Testing Files .spec.ts files were auto generated by angular CLI when I created them. However these are just files for defining ENUM statuses so they don't need the test files.

--- src/app/computerdifficulty.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Computerdifficulty } from './computerdifficulty';
-
-describe('Computerdifficulty', () => {
-  it('should create an instance', () => {
-    expect(new Computerdifficulty()).toBeTruthy();
-  });
-});
--- src/app/sound-toggle.spec.ts
+++
@@ -1,7 +0,0 @@
-import { SoundToggle } from './sound-toggle';
-
-describe('SoundToggle', () => {
-  it('should create an instance', () => {
-    expect(new SoundToggle()).toBeTruthy();
-  });
-});
